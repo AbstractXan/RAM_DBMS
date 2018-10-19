c970dc6ca0a131705496ef1033970fc96c092605
RohithS98 <rohith.sudheer@gmail.com>
2018-10-19 22:43:22 +0530

    Added types

--- src/handle.h
+++ src/handle.h
@@ -10,12 +10,42 @@
 
 typedef enum { STR, VAL } attrtype_t;
 typedef enum { SUCC, FAIL } resmsg_t;
+typedef struct cell_t cell_t;
+typedef struct attr_t attr_t;
+typedef struct table_t table_t;
+typedef struct database_t database_t;
 
 typedef union {
 	char str[MAXSTR];
 	float value;
 }attrval_t;
 
+struct cell_t {
+	attrval_t value;
+	cell_t *next;
+};
+
+struct attr_t {
+	char *attr_names;
+	cell_t *cell;
+	int attr_type;
+	bool isPK;
+	bool isFK;
+	bool isNotNull;
+	bool hasDefault;
+	attrval_t defaultVal;
+};
+
+struct table_t{
+	int rows;
+	int cols;
+	attr_t *attributeList;
+	table_t *nextTable;
+};
+
+struct database_t {
+	table_t *firstTable;
+};
 
 typedef struct {
 	resmsg_t status;
@@ -32,7 +62,3 @@ typedef struct {
 }handle_t;
 
 extern handle_t* get_handle();
-
-
-
-
